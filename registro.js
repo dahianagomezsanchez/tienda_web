1b87c31c8503b880d33de1edf8d90628feb9a0f7
acaloatoikio <jcarlogiraldo@gmail.com>
2021-09-24 16:44:59 -0500

    fix validación corregido

--- registro.js
+++ registro.js
@@ -4,49 +4,42 @@ var contraconfirm = document.getElementById("contraconfirm");
 var expCorreo = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
 var expcontrasena = /^(?=.*[A-Za-z])(?=.\d)[A-Za-z\d]{8,}$/;
 
-function limpiarErrores(){
-
+function limpiarErrores() {
     var error = document.getElementsByName("error");
-    for (var i = 0; i< error.length; i++){
-
-        error[i].innerHTML= "";
+    for (var i = 0; i < error.length; i++) {
+        error[i].innerHTML = "";
     }
-
 }
 
-
-function validacion(formulario){
+function validacion(formulario) {
     limpiarErrores();
-    if (!expCorreo.test(formulario.correo.value)){
-        document.getElementById("ErrorCorreo").innerText="El correo debe contener @ y un dominio"; 
+    if (!expCorreo.test(formulario.correo.value)) {
+        document.getElementById("ErrorCorreo").innerText =
+            "El correo debe contener @ y un dominio";
 
         formulario.correo.focus();
 
         return false;
     }
 
-
-    if (!expcontrasena.test(formulario.contrasena.value)){
-
-        document.getElementById("ErrorContrasena").innerText="La contaseña debe contener 8 carácteres entre letras y números";
+    if (!expcontrasena.test(formulario.contrasena.value)) {
+        document.getElementById("ErrorContrasena").innerText =
+            "La contaseña debe contener 8 carácteres entre letras y números";
 
         formulario.contrasena.focus();
 
         return false;
-
-
     }
-    if (formulario.contrasena.value != contraconfirm){ 
-        document.getElementById("ErrorConfirmarContrasena").innerText="Las contraseñas no coinciden";
-    
+    if (formulario.contrasena.value != formulario.contraconfirm.value) {
+        document.getElementById("ErrorConfirmarContrasena").innerText =
+            "Las contraseñas no coinciden";
+
         formulario.contraconfirm.focus();
         return false;
+    } else {
+        return true;
     }
-
-
-
-        else {return true;}
-        alert("Su registro se ha realizado satisfactoriamente")
-
-    
+    alert("Su registro se ha realizado satisfactoriamente");
 }
+
+document.formulario.submit();
