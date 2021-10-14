209d0e44ad3c843d386d88df07691110c3554b4c
acaloatoikio <jcarlogiraldo@gmail.com>
2021-10-14 11:52:08 -0500

    "validación roles"

---
+++ misiontic-shop/src/shared/components/forbiden/ForbidenComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ForbidenComponent = () =>{
+    return (
+        <h1>
+            "It is not allowed to enter this area"
+        </h1>
+    )
+}
+
+export default ForbidenComponent;
