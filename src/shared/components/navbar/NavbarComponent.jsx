52b91793ef3f0a4dbcfe1b8b4459cb416646c8e6
acaloatoikio <jcarlogiraldo@gmail.com>
2021-10-14 21:11:30 -0500

    ocultar log in cuando esta autenticado

--- src/shared/components/navbar/NavbarComponent.jsx
+++ src/shared/components/navbar/NavbarComponent.jsx
@@ -14,13 +14,13 @@ function NavbarComponent(props) {
   let title1 = props.title1;
 
   return (
-  
-    
-    
+
+
+
     <nav id="header" className="navbar navbar-light navbar-expand-lg navbar-dark bg-dark">
-          
+
       <div className="container-fluid">
-        
+
         <a className="navbar-brand" href="#">
           <img
             src={logo}
@@ -30,9 +30,9 @@ function NavbarComponent(props) {
             class="d-inline-block align-text-right-top"
 
           />
-          <h5 >{title}-{ title1}</h5>
-         
-                           
+          <h5 >{title}-{title1}</h5>
+
+
         </a>
         <button
           className="navbar-toggler"
@@ -47,43 +47,43 @@ function NavbarComponent(props) {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar ms-auto mb-2 mb-lg-3">
-            
+
             <li className="nav-item">
               <Link to="/home" className="nav-link text-login">Home</Link>
             </li>
-           
-              {/* <Link to = "/login" className="nav-link text-login" >Login</Link> */}
-            <a       
-                type="button"
-                className="nav-link text-login"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-              aria-label="Toggle navigation" onClick={() => loginWithRedirect()}>Log In</a>;
-          
-            {isAuthenticated ? <a  type="button"
+
+            {/* <Link to = "/login" className="nav-link text-login" >Login</Link> */}
+            {isAuthenticated ? null : <a
+              type="button"
               className="nav-link text-login"
               data-bs-toggle="collapse"
               data-bs-target="#navbarSupportedContent"
               aria-controls="navbarSupportedContent"
               aria-expanded="false"
-                aria-label="Toggle navigation" onClick={() => logout({ returnTo: window.location.origin })}>
+              aria-label="Toggle navigation" onClick={() => loginWithRedirect()}>Log In</a>};
+
+            {isAuthenticated ? <a type="button"
+              className="nav-link text-login"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation" onClick={() => logout({ returnTo: window.location.origin })}>
               Log Out  </a> : null};
-            
+
             <li className="nav-item">
-              <Link to = "/users" className="nav-link">Users</Link>
+              <Link to="/users" className="nav-link">Users</Link>
             </li>
             <li className="nav-item">
               <Link to="/sales" className="nav-link active" aria-current="page">Sales</Link>
-              </li>
+            </li>
             <li className="nav-item">
               <Link to="/register" className="nav-link active" aria-current="page">Register</Link>
             </li>
-             <a>
+            <a>
               <button class="btn btn-outline-success" type="submit">{isAuthenticated ? user.name : "User"}</button>
             </a>
-        
+
           </ul>
         </div>
       </div>
